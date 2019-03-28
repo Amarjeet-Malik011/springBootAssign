245be80dff5f17097c829d470a5daf380cda3f19
Amarjeet-Malik011 <ch.amarjeetmalik@gmail.com>
2019-03-28 14:27:38 +0530

    minor improve

--- src/main/java/com/ttn/springBootAssign/entity/Bootstrap.java
+++ src/main/java/com/ttn/springBootAssign/entity/Bootstrap.java
@@ -1,7 +1,10 @@
 package com.ttn.springBootAssign.entity;
 
+import com.ttn.springBootAssign.controller.DatabaseController;
 import com.ttn.springBootAssign.repository.StudentRepository;
 import com.ttn.springBootAssign.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
@@ -14,15 +17,18 @@ public class Bootstrap {
     // Q6...Bootstrap Data for Student Domain
     @Autowired
     StudentRepository studentRepository;
+    Logger logger= LoggerFactory.getLogger(Bootstrap.class);
+
     @EventListener(ApplicationStartedEvent.class)
     public void init()
     {
+        logger.info("bootstraping data");
         Iterator<Student> bookIterable = studentRepository.findAll().iterator();
         if (!bookIterable.hasNext()) {
             for (int i = 1; i <= 10; i++) {
-                Student book = new Student(i, "Book " + i,10.0f);
-                studentRepository.save(book);
-                System.out.println("Book " + i + " created");
+                Student student = new Student(i, "Book " + i,10.0f);
+                studentRepository.save(student);
+                System.out.println("Student " + i + " created");
             }
         }
         System.out.println("Your Application is up and running");
